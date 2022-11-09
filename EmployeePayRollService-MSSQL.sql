abcf7814a29b28f1062b7346622e0ed49f0b376c
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-11-09 10:35:53 +0530

    [Pratu]ADDED: Create a Employee PayRoll Table

--- EmployeePayRollService-MSSQL.sql
+++ EmployeePayRollService-MSSQL.sql
@@ -1,2 +1,8 @@
 --UC1-Create Database--
 CREATE DATABASE PayRoll_Services;
+
+--UC2 - Create employee_payroll table
+create table employee_payroll( ID int primary key not null identity(1,1), NameofStudent varchar (150) not null,Salary float not null,StartDate date);
+
+select * from employee_payroll
+
