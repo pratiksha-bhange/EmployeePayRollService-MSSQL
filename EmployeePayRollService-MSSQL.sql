ccfa523a09bbf0cf71a542086debaf2f56cd845c
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-11-09 11:22:46 +0530

    [Pratu]ADDED: Retrive Particular Data From Employee PayRoll Table

--- EmployeePayRollService-MSSQL.sql
+++ EmployeePayRollService-MSSQL.sql
@@ -15,3 +15,8 @@ insert into employee_payroll (nameofStudent, Salary, StartDate) values
 
 --UC4 - Retrieve employee_payroll data
 select * from employee_payroll
+
+--UC5 - Retrieve salary of particular employee and in particular date range.
+select salary from employee_payroll where nameofStudent = 'Bill';
+select * from employee_payroll
+where StartDate between CAST ('2018-01-01' as date) and GETDATE()
